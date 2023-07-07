a15f53af50f7ae60e1b3abc1c39387a3880d4149
Usman Nasution <usmannasty32@gmail.com>
2023-07-07 15:24:24 +0700

    updated userseeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,12 +4,18 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder {
   /**
    * Run the database seeds.
    */
   public function run(): void {
-    //
+    DB::table('users')->insert([
+      'name' => 'ams_admin',
+      'email' => 'ams_admin@gmail.com',
+      'password' => Hash::make('ams_admin')
+    ]);
   }
 }
