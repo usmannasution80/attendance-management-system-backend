e1db9cc9982ab1a7228b0ee30c0037ec9d93bb90
Usman Nasution <usmannasution32@gmail.com>
2024-06-01 21:53:21 +0700

    Add create_random_users to artisan command

--- routes/console.php
+++ routes/console.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
+use App\Custom\Constants;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,4 +28,33 @@ Artisan::command('create_admin {name} {email} {password}', function($name, $emai
   $user->password = Hash::make($password);
   $user->save();
   echo 'New admin created!';
+});
+
+Artisan::command('generate_random_users', function(){
+  $departments = array_keys(Constants::DEPARTMENTS);
+  $grade = 10;
+  while($grade <= 12){
+    $sql = 'INSERT INTO '.env('DB_DATABASE', 'ams').'.users (name, grade, department, class) VALUES ';
+    foreach($departments as $department){
+      $count = 1;
+      while($count <= Constants::DEPARTMENTS[$department] * 35){
+        $class = ceil($count / 35);
+        $sql .= '('
+              . "'John $grade $department $class ___',"
+              . "'$grade',"
+              . "'$department',"
+              . "'$class'),";
+        $count++;
+      }
+    }
+    $sql = preg_replace('/,$/i', '', $sql);
+    $con = mysqli_connect(
+      env('DB_HOST'),
+      env('DB_USERNAME'),
+      env('DB_PASSWORD'),
+      env('DB_DATABASE')
+    );
+    mysqli_query($con, $sql);
+    $grade++;
+  }
 });
